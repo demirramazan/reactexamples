413c359315cb2576b427ddf2b4f0639409242298
ramazan.demir <ramazan.demir9@icisleri.gov.tr>
2021-02-19 23:38:20 +0300

    udemy react context kursu örnek uygulama yapıldı.

--- react_redux_example/src/index.js
+++ react_redux_example/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import LiftingStateUp from './LiftingStateUp';
+import LiftingStateUp from './LiftingStateUp';
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import  {reducer} from './reducer'
